02b7262977f076307e6f8f2292b859cfe1277aa2
Nyade <niinyade@icloud.com>
2022-05-11 18:16:47 -0300

    Add the first API code to the project, still trying to understand delegates, protocols and to apply API.

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,43 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Nyade on 11/05/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=f7ff04497946561b2f78f0461edd56a2&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error:))
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
+    }
+    
+}
